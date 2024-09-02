0498ce0094158964b3529d5d02e8625d13413d07
OmBhavsar161 <ombhavsar2004@gmail.com>
2024-09-02 15:59:39 +0530

    routes

--- backend/index.js
+++ backend/index.js
@@ -45,9 +45,28 @@ app.get("/", (req, res) => {
   res.send("Express App is Running");
 });
 
+// // Image Storage Engine
+// const storage = multer.diskStorage({
+//   destination: "./upload/images",
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+//     );
+//   },
+// });
+// const upload = multer({ storage: storage });
 
+// // Serve Uploaded Images
+// app.use("/images", express.static("upload/images"));
 
-
+// // Image Upload Endpoint
+// app.post("/upload", upload.single("product"), (req, res) => {
+//   res.json({
+//     success: 1,
+//     image_url: `http://localhost:${port}/images/${req.file.filename}`,
+//   });
+// });
 
 // Product Schema
 const Product = mongoose.model("Product", {
